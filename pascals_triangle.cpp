5eed2caab760d47d3134f1f66835f714cdb5d88c
Ritaban <ritabandatta2000@gmail.com>
2025-07-09 09:01:09 +0530

    updated pascals triangle

--- pascals_triangle.cpp
+++ pascals_triangle.cpp
@@ -1,20 +1,14 @@
 class Solution {
     public:
         vector<vector<int>> generate(int numRows) {
-            vector<vector<int>> triangle;
-    
-            for (int i = 0; i < numRows; ++i) {
-                vector<int> row(i + 1, 1);  // Initialize row with 1s
-    
-                // Fill inner values using values from previous row
-                for (int j = 1; j < i; ++j) {
-                    row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
-                }
-    
-                triangle.push_back(row);  // Append the row to the triangle
+            vector<vector<int>> arr;
+            for(int i=0; i<numRows;++i ){
+                vector<int> row(i+1,1);
+                for(int j=1; j<i;++j ){
+                        row[j]=arr[i-1][j-1]+arr[i-1][j];
+                }   
+                arr.push_back(row);
             }
-    
-            return triangle;
+            return arr;
         }
-    };
-    
+    };  
