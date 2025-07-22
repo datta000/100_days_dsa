024a8ef8a89b9430e90bbe71e52ed5ee59766b06
Ritaban <ritabandatta2000@gmail.com>
2025-07-22 23:43:23 +0530

    solved

--- next_permutation.js
+++ next_permutation.js
@@ -1,32 +1,25 @@
-function nextPermutation(nums) {
-    let n = nums.length;
-    let i = n - 2;
-  
-    // Step 1: Find the first decreasing element from the end
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var nextPermutation = function(nums) {
+    
+    let size = nums.length;
+    let i = size - 2;
     while (i >= 0 && nums[i] >= nums[i + 1]) {
-      i--;
+        i--;
     }
-  
     if (i >= 0) {
-      // Step 2: Find the next greater element to swap with
-      let j = n - 1;
-      while (j >= 0 && nums[j] <= nums[i]) {
-        j--;
-      }
-      // Step 3: Swap them
-      [nums[i], nums[j]] = [nums[j], nums[i]];
+        let j = size - 1;
+        while (nums[j] <= nums[i]) {
+            j--;
+        }
+        [nums[i], nums[j]] = [nums[j], nums[i]];
     }
-  
-    // Step 4: Reverse the suffix
-    reverse(nums, i + 1, n - 1);
-  }
-  
-  // Helper function to reverse part of the array in-place
-  function reverse(arr, start, end) {
-    while (start < end) {
-      [arr[start], arr[end]] = [arr[end], arr[start]];
-      start++;
-      end--;
+    let start = i+1, end =size-1;
+    while(start<end){
+        [nums[start],nums[end]]=[nums[end],nums[start]]
+        start++
+        end--
     }
-  }
-  
+};
