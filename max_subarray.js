b0b78a9b1f9ae0e91029234bc1c5214286d8bf94
Ritaban <ritabandatta2000@gmail.com>
2025-07-23 10:41:39 +0530

    maxSubarray done

---
+++ max_subarray.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let maxSum= nums[0];
+    let currentSum= nums[0];
+    for(i=1;i<nums.length;i++){
+        currentSum= Math.max(nums[i], currentSum+nums[i])
+        maxSum= Math.max(maxSum, currentSum)
+    }
+    return maxSum;
+};
