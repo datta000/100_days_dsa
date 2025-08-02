c9fbf0a45baa0156eb3816c4e6c515ac60b3e018
Ritaban <ritabandatta2000@gmail.com>
2025-08-03 00:00:16 +0530

    partitioning

--- palindrome_part.js
+++ palindrome_part.js
@@ -1,10 +1,11 @@
 /**
- * @param {string} 
+ * @param {string} s
  * @return {string[][]}
  */
 var partition = function (s) {
     const result = [];
 
+    // Helper to check if a string is a palindrome between indices left and right
     function isPalindrome(str, left, right) {
         while (left < right) {
             if (str[left] !== str[right]) return false;
@@ -14,17 +15,20 @@ var partition = function (s) {
         return true;
     }
 
+    // Backtracking function
     function backtrack(start, path) {
+        // If we reach the end of the string, we add the current path to result
         if (start === s.length) {
-            result.push([...path]); // Reached the end, save partition
+            result.push([...path]);
             return;
         }
 
+        // Try all possible partitions starting from index `start`
         for (let end = start; end < s.length; end++) {
             if (isPalindrome(s, start, end)) {
-                path.push(s.slice(start, end + 1)); // Choose the palindrome
-                backtrack(end + 1, path); // Explore
-                path.pop(); // Backtrack
+                path.push(s.slice(start, end + 1)); // Choose
+                backtrack(end + 1, path);           // Explore
+                path.pop();                         // Backtrack
             }
         }
     }
