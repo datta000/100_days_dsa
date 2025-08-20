0ed663d29fd5d6c5c9bc1f220c6b5f9a8bb1fc45
Ritaban <ritabandatta2000@gmail.com>
2025-08-20 23:51:30 +0530

    first

---
+++ first_occur.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+    let n = haystack.length;
+    let m = needle.length;
+
+    for (let i = 0; i <= n - m; i++) {
+        if (haystack.substring(i, i + m) === needle) {
+            return i;
+        }
+    }
+    return -1;
+};
