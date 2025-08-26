2c4512e08918637d37dabdcab04b36cf19415631
Ritaban <ritabandatta2000@gmail.com>
2025-08-26 23:50:15 +0530

    better time complexity

--- first_occur.js
+++ first_occur.js
@@ -6,7 +6,7 @@
 var strStr = function (haystack, needle) {
     let n = haystack.length;
     let m = needle.length;
-
+    if (m > n) return -1
     for (let i = 0; i <= n - m; i++) {
         if (haystack.substring(i, i + m) === needle) {
             return i;
