f6fed9866dbc4c420631b9d872ffd24dc03f0ef7
Ritaban <ritabandatta2000@gmail.com>
2025-08-30 09:54:20 +0530

    rabin karp

---
+++ rabin_karp.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {number}
+ */
+var repeatedStringMatch = function (a, b) {
+    let str = "";
+    let n = Math.ceil(b.length / a.length);
+    str = a.repeat(n);
+    if (str.includes(b)) {
+        return n;
+    }
+    str += a;
+    if (str.includes(b)) return n + 1;
+    return -1;
+};
