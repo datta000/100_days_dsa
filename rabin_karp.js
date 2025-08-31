cfeb14c95a9bd65ae3d050974092d30284b1ed1b
Ritaban <ritabandatta2000@gmail.com>
2025-09-01 00:00:56 +0530

    middle of linked list

--- rabin_karp.js
+++ rabin_karp.js
@@ -7,9 +7,7 @@ var repeatedStringMatch = function (a, b) {
     let str = "";
     let n = Math.ceil(b.length / a.length);
     str = a.repeat(n);
-    if (str.includes(b)) {
-        return n;
-    }
+    if (str.includes(b)) {return n;}
     str += a;
     if (str.includes(b)) return n + 1;
     return -1;
